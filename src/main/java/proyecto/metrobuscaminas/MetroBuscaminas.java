1325303d9d324a7ec2cca49f427b6f4109ec2f46
SIMONANANIAN <simonananian2006@gmail.com>
2025-03-02 17:39:32 -0400

    Se despliega en la interfaz la configuración de filas y columnas y la cantidad de minas

--- src/main/java/proyecto/metrobuscaminas/MetroBuscaminas.java
+++ src/main/java/proyecto/metrobuscaminas/MetroBuscaminas.java
@@ -6,15 +6,13 @@ package proyecto.metrobuscaminas;
  * @version 1.0
  * @since 2025-02-22
  */
+import proyecto.metrobuscaminas.tablero.Tablero;
+
 public class MetroBuscaminas {
 
-    /**
-     * Método principal que sirve como punto de entrada para la ejecución de la aplicación.
-     *
-     * @param args Argumentos de la línea de comandos.
-     */
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Probando el merge");
+        Tablero Ventana = new Tablero();
+        
     }
 }
