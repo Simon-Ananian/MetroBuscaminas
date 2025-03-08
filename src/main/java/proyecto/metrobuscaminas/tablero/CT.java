718d6ba18a88ca0d2e8cf300c244a6a810a579c2
SIMONANANIAN <simonananian2006@gmail.com>
2025-03-08 19:27:35 -0400

    Solucionado un error en la asignación repetida de minas

--- src/main/java/proyecto/metrobuscaminas/tablero/CT.java
+++ src/main/java/proyecto/metrobuscaminas/tablero/CT.java
@@ -18,14 +18,32 @@ public class CT {
     public void generarMinas() {
         int numMinas = Math.max(filas, columnas);
         Random rand = new Random();
-
+        String[] posicionesMinas = new String[numMinas];
+        
         for (int i = 0; i < numMinas; i++) {
             byte fila = (byte) rand.nextInt(filas);
             byte col = (byte) rand.nextInt(columnas);
-            Nodo<Byte> mina = new Nodo<>((byte) '*', fila, col);
-            grafo.insertaVertice(mina);
+            String clave = fila + "," + col;
+            
+            boolean repetida = false;
+            for (int j = 0; j < i; j++) { // Se recorre hasta el índice actual
+                if (posicionesMinas[j].equals(clave)) {
+                    repetida = true;
+                    break;
+                }
+            }
+            
+            if(repetida){
+                i--; // Si la posición ya existe, restamos 1 a i para repetir la iteración
+            } else {
+                posicionesMinas[i] = clave; // Guardamos la nueva posición en el arreglo
+                Nodo<Byte> mina = new Nodo<>((byte) '*', fila, col);
+                grafo.insertaVertice(mina); // Insertamos la mina en el grafo
+            }
+  
         }
     }
+
     
     public String[][] generarMatriz(int filas, int columnas) {
         CT controlador = new CT(filas, columnas);
